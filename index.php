bc5b727e9f9e0177bde2cdf010a9239ee609207d
jvoorschot <j.j.v.oorschot@student.tue.nl>
2017-08-25 11:49:41 +0200

    updates

--- index.php
+++ index.php
@@ -1,4 +1,59 @@
 <?php
 /**
  * Jeroen van Oorschot 2017
+ * Reads a list of folders, and shows its subfolders as hyperlinks.
  */
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Photo index</title>
+        <link href="css/metro.min.css" rel="stylesheet">
+        <link href="css/custom.css" rel="stylesheet">
+</head>
+<body>
+
+<div id="wrapper">
+<h1>Photo index</h1>
+ <div class="accordion" data-role='accordion'>
+<?
+   $allfolders = glob('pictures/*', GLOB_ONLYDIR);
+   sort($allfolders);
+   $numfolders = count($allfolders);
+       //look if there is a file named 'desc.txt' that contains the descriptions for each image
+   if ($numfolders == 0) {
+           echo 'No folders';
+       } else {
+           for ($f = 0; $f < $numfolders; $f++) {
+             $thismainfolder = allfolders[$f];
+             ?>
+              <div class="frame">
+                <div class="heading"><?php echo $thismainfolder; ?></div>
+                <div class="content"><?php
+                $subfolders = glob('pictures/'.$thismainfolder.'/*', GLOB_ONLYDIR);
+                $numsubfolders = count($subfolders);
+                if($numsubfolders == 0){
+                  echo 'No subfolders';
+                }else{
+                  for($s=0; $s < $numsubfolders; $s++){
+                    ?>
+                      <a href="photo.php?main=<?php echo $thismainfolder; ?>&amp;sub=<?php echo $s; ?>" title="<?php echo $subfolders[$s]; ?>"><?php echo $subfolders[$s]; ?></a>
+                    <?php
+                  }
+                }
+
+                </div>
+              </div>
+
+            <?php    }
+        }
+        ?>
+</div>
+
+    <script src="js/jquery-3.2.1.min.js"></script>
+    <script src="js/metro.min.js"></script>
+
+
+</body>
+</html>
--- photo.php
+++ photo.php
@@ -6,39 +6,34 @@
             background-color: #bbb;
         }
     </style>
+    <link href="css/lightbox.css" rel="stylesheet">
+    <link href="css/metro.min.css" rel="stylesheet">
+    <link href="css/custom.css" rel="stylesheet">
+
 </head>
 <body>
-
+<div id="container">
 <?php
 /**
  * Jeroen van Oorschot 2017
  */
- 
-echo '<div id="fotopagina">';
-//get $mainfolder, array of top-level folders to use, (lente, zomer, herfst, winter, history..... , machines)
-//get $subfolder,  array of wich subfolders to use and in what order. onderwerpen en gewassen
-if (isset($_GET['folder'])) {
-    $n = $_GET['folder'];
-    $allfolders = glob('*', GLOB_ONLYDIR);
-    asort($allfolders);
-    $thismainfolder = (array_values($allfolders))[$n];
-    echo "<div class=\"imgblock\"><h3>$thismainfolder</h3>";
-    /*
-    $headdescfileaddr = "beelden/$thismainfolder/headdesc.txt";
-    $headdescfile = file_exists($headdescfileaddr)?fopen($headdescfileaddr,'r'):NULL;
-    if($headdescfile){
-        $line = fgets($descfile);
-        while($line != false){
-            $line = fgets($descfile);
-            echo $line;
-        }
-    }*/
+
+if (isset($_GET['main'])) {
+  /* Mainfolder is obtained using the name of the folder as 'main' get string
+     Subfolder is obtained as directory index of the folders in the mainfolder.
+     Photos are pulled from 'pictures/mainfolder/subfolder/*.jpg'.
+     */
+    $thismainfolder = $_GET['main'];
+    $n = $_GET['sub'];
+    $subfolders = glob('pictures/'.$thismainfolder.'/*', GLOB_ONLYDIR);
+    asort($subfolders);
+    $thissubfolder = (array_values($subfolders))[$n];
+    echo "<div class=\"imgblock\"><h3>$thismainfolder -- $thissubfolder</h3>";
+
     //$filenames is an array of all filenames in 'location' with .jpg as extension
     //Glob is a function to obtain all files in a specified folder ($location), with a specified filename (*, so not specified here) and extension (.jpg)
-    //if(isset($_GET['noCache'])||!file_exists($location.'.txt')){
-#	echo __DIR__;
-#echo $thismainfolder;
-    $filenames = glob($thismainfolder.'/*.jpg', GLOB_NOSORT);
+
+    $filenames = glob('pictures/'.$thismainfolder.'/'.$thissubfolder.'/*.jpg', GLOB_NOSORT);
     asort($filenames);
     $filenames = (array_values($filenames));
     $numimgs = count($filenames);
@@ -55,15 +50,13 @@ if (isset($_GET['folder'])) {
             //Both image start with the same filename, but the small one ends with a 's' and the large one ends with 'l'
             // Because $filenames is in alphabetic order, the large one is just in front of the small one
             $largeimgsrc = $filenames[$pic];
-            $smallimgsrc = $largeimgsrc;
-#            $smallimgsrc = '/' . $filenames[$pic + 1];
-            //if there is a file with a description for each image.
-            //Every line in the textfile is the description for one image, in alphabetical order.
-            $alt = $thismainfolder;
+            $smallimgsrc = $filenames[$pic+1];
+
+            $alt = $thissubfolder;
             $desconpage = NULL;
             $title = NULL;
             echo "
-			<a href=\"$largeimgsrc\" rel=\"lightbox\" class=\"html5lightbox\" data-group=\"$thismainfolder\" $title>
+			<a href=\"$largeimgsrc\" data-lightbox=\"$thismainfolder\" $title>
 				<div class=\"floatimg\" $style>
 						<img src=\"$smallimgsrc\" alt=\"$alt\"	 />
 				</div>
@@ -74,9 +67,11 @@ if (isset($_GET['folder'])) {
     }
 }else{
     echo 'Invalid folder supplied.';
-}
-echo '</div>';
-echo '</div>';
-?>
+}?>
+  </div>
+</div>
+<script src="js/jquery-3.2.1.min.js"></script>
+<script src="js/metro.min.js"></script>
+<script src="js/lightbox.js"></script>
 </body>
 </html>
