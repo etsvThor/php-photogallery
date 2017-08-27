f3f7eaaa2fd244cf2eed7be3204f47d54b8835e2
JeroenvO <j.j.v.oorschot@student.tue.nl>
2017-08-27 17:34:39 +0200

    bugfixes

--- index.php
+++ index.php
@@ -9,46 +9,47 @@
 <head>
     <meta charset="UTF-8">
     <title>Photo index</title>
-        <link href="css/metro.min.css" rel="stylesheet">
-        <link href="css/custom.css" rel="stylesheet">
+    <link href="css/metro.min.css" rel="stylesheet">
+    <link href="css/custom.css" rel="stylesheet">
 </head>
 <body>
 
 <div id="wrapper">
-<h1>Photo index</h1>
- <div class="accordion" data-role='accordion'>
-<?
-   $allfolders = glob('pictures/*', GLOB_ONLYDIR);
-   $numfolders = count($allfolders);
-       //look if there is a file named 'desc.txt' that contains the descriptions for each image
-   if ($numfolders == 0) {
-           echo 'No folders';
-       } else {
-           for ($f = 0; $f < $numfolders; $f++) {
-             $thismainfolder = allfolders[$f];
-             ?>
-              <div class="frame">
-                <div class="heading"><?php echo $thismainfolder; ?></div>
-                <div class="content"><?php
-                $subfolders = glob('pictures/'.$thismainfolder.'/*', GLOB_ONLYDIR);
-                $numsubfolders = count($subfolders);
-                if($numsubfolders == 0){
-                  echo 'No subfolders';
-                }else{
-                  for($s=0; $s < $numsubfolders; $s++){
-                    ?>
-                      <a href="photo.php?main=<?php echo $f; ?>&amp;sub=<?php echo $s; ?>" title="<?php echo $subfolders[$s]; ?>"><?php echo $subfolders[$s]; ?></a>
-                    <?php
-                  }
-                }
-
+    <h1>Photo index</h1>
+    <div class="accordion" data-role='accordion'>
+        <?
+        $allfolders = glob('pictures/*', GLOB_ONLYDIR);
+        $numfolders = count($allfolders);
+        //look if there is a file named 'desc.txt' that contains the descriptions for each image
+        if ($numfolders == 0) {
+            echo 'No folders';
+        } else {
+            for ($f = 0; $f < $numfolders; $f++) {
+                $thismainfolder = $allfolders[$f];
+                ?>
+                <div class="frame">
+                    <div class="heading"><?php echo $thismainfolder; ?></div>
+                    <div class="content"><?php
+                        $subfolders = glob($thismainfolder . '/*', GLOB_ONLYDIR);
+                        $numsubfolders = count($subfolders);
+                        if ($numsubfolders == 0) {
+                            echo 'No subfolders';
+                        } else {
+                            for ($s = 0; $s < $numsubfolders; $s++) {
+                                ?>
+                                <a href="photo.php?main=<?php echo $f; ?>&amp;sub=<?php echo $s; ?>"
+                                   title="<?php echo $subfolders[$s]; ?>"><?php echo $subfolders[$s]; ?></a>
+                                <?php
+                            }
+                        }
+                        ?>
+                    </div>
                 </div>
-              </div>
-
-            <?php    }
+                <?php
+            }
         }
         ?>
-</div>
+    </div>
 
     <script src="js/jquery-3.2.1.min.js"></script>
     <script src="js/metro.min.js"></script>
--- photo.php
+++ photo.php
@@ -1,4 +1,3 @@
-
 <html>
 <head>
     <title>Waldur foto archief</title>
@@ -14,65 +13,64 @@
 </head>
 <body>
 <div id="container">
-<?php
-/**
- * Jeroen van Oorschot 2017
- */
-
-if (isset($_GET['main'])) {
-  /* Mainfolder is obtained as directory index of the root folder '/pictures/'
-     Subfolder is obtained as directory index of the folders in the mainfolder.
-     Photos are pulled from 'pictures/mainfolder/subfolder/*.jpg'.
+    <?php
+    /**
+     * Jeroen van Oorschot 2017
      */
+
+    if (isset($_GET['main'])) {
+    /* Mainfolder is obtained as directory index of the root folder '/pictures/'
+       Subfolder is obtained as directory index of the folders in the mainfolder.
+       Photos are pulled from 'pictures/mainfolder/subfolder/*.jpg'.
+       */
     $m = $_GET['main'];
     $n = $_GET['sub'];
 
     $mainfolders = glob('pictures/*', GLOB_ONLYDIR);
     $thismainfolder = (array_values($mainfolders))[$m];
 
-    $subfolders = glob('pictures/'.$thismainfolder.'/*', GLOB_ONLYDIR);
+    $subfolders = glob($thismainfolder . '/*', GLOB_ONLYDIR);
     $thissubfolder = (array_values($subfolders))[$n];
-    echo "<div class=\"imgblock\"><h3>$thismainfolder -- $thissubfolder</h3>";
+    ?>
+    <div class="imgblock"><h3><?php echo $thismainfolder . '--' . $thissubfolder; ?></h3>";
+        <?php
+        //$filenames is an array of all filenames in 'location' with .jpg as extension
+        //Glob is a function to obtain all files in a specified folder ($location), with a specified filename (*, so not specified here) and extension (.jpg)
 
-    //$filenames is an array of all filenames in 'location' with .jpg as extension
-    //Glob is a function to obtain all files in a specified folder ($location), with a specified filename (*, so not specified here) and extension (.jpg)
+        $filenames = glob($thismainfolder . '/' . $thissubfolder . '/*.jpg');
+        $filenames = (array_values($filenames));
+        $numimgs = count($filenames);
+        //look if there is a file named 'desc.txt' that contains the descriptions for each image
+        if ($numimgs == 0) {
+            echo 'Geen foto\'s';
+        } else {
+            $alt = "";
+            $desconpage = "";
+            $style = "";
+            for ($pic = 0; $pic < $numimgs; $pic = $pic + 2) {
+                //build the html for this img. The first image is the large one 800*600px, the second is the small one 200*150px.
+                //this works if every image has a unique name, and is both in small and large format in the folder.
+                //Both image start with the same filename, but the small one ends with a 's' and the large one ends with 'l'
+                // Because $filenames is in alphabetic order, the large one is just in front of the small one
+                $largeimgsrc = $filenames[$pic];
+                $smallimgsrc = $filenames[$pic + 1];
 
-    $filenames = glob('pictures/'.$thismainfolder.'/'.$thissubfolder.'/*.jpg');
-    $filenames = (array_values($filenames));
-    $numimgs = count($filenames);
-    //look if there is a file named 'desc.txt' that contains the descriptions for each image
-    if ($numimgs == 0) {
-        echo 'Geen foto\'s';
-    } else {
-        $alt = "";
-        $desconpage = "";
-        $style = "";
-        for ($pic = 0; $pic < $numimgs; $pic = $pic + 2) {
-            //build the html for this img. The first image is the large one 800*600px, the second is the small one 200*150px.
-            //this works if every image has a unique name, and is both in small and large format in the folder.
-            //Both image start with the same filename, but the small one ends with a 's' and the large one ends with 'l'
-            // Because $filenames is in alphabetic order, the large one is just in front of the small one
-            $largeimgsrc = $filenames[$pic];
-            $smallimgsrc = $filenames[$pic+1];
-
-            $alt = $thissubfolder;
-            $desconpage = NULL;
-            $title = NULL;
-            echo "
+                $alt = $thissubfolder;
+                $desconpage = NULL;
+                $title = NULL;
+                echo "
 			<a href=\"$largeimgsrc\" data-lightbox=\"$thismainfolder\" $title>
 				<div class=\"floatimg\" $style>
 						<img src=\"$smallimgsrc\" alt=\"$alt\"	 />
 				</div>
 			</a>
-
-
 			";
+            }
         }
-    }
-}else{
-    echo 'Invalid folder supplied.';
-}?>
-  </div>
+        } else {
+            echo 'Invalid folder supplied.';
+        } ?>
+    </div>
 </div>
 <script src="js/jquery-3.2.1.min.js"></script>
 <script src="js/metro.min.js"></script>
